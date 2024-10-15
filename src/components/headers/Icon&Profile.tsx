eb046b8a62685191f8c000e4a78b39a702588d01
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-15 21:08:07 +0200

    icon

--- src/components/headers/Icon&Profile.tsx
+++ src/components/headers/Icon&Profile.tsx
@@ -2,15 +2,15 @@ import { SignInButton } from "@/features/auth/SignInButton"
 import {Users2, MessageCircleMore, Bell} from "lucide-react"
 
 export const IconAndProfile = ({session}: {session: any}) => {
-  return <div className="flex items-center gap-7">
-      <Users2 size={22} className="cursor-pointer text-gray-400"/>
-      <MessageCircleMore size={22} className="cursor-pointer text-gray-400"/>
-      <Bell size={22} className="cursor-pointer text-gray-400"/>
+  return <div className="flex items-center gap-6">
+      <Users2 size={22} className="cursor-pointer text-gray-400 hover:text-purple-400 duration-75"/>
+      <MessageCircleMore size={22} className="cursor-pointer text-gray-400 hover:text-purple-400 duration-75"/>
+      <Bell size={22} className="cursor-pointer text-gray-400 hover:text-purple-400 duration-75"/>
         
       {!session ? (
         <SignInButton variant="default" bgColor="bg-purple-400" />
       ) : (
-        null
+        <img src={session.user.image} alt="profile" width={28} height={28} loading="lazy" className="rounded-full cursor-pointer" />
       )}
     </div>
 }
