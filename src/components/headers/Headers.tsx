437e3c36d8015d613fbe4f281a458acb0d7533cd
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-15 21:01:14 +0200

    searchbar

--- src/components/headers/Headers.tsx
+++ src/components/headers/Headers.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { Links } from './Links'
+import { SearchBar } from './SearchBar'
 
 export const Headers = () => {
   return <header className="h-[85px] shadow-md bg-[#181616]">
@@ -7,8 +8,9 @@ export const Headers = () => {
         <Link href="/">
             <h2 className="text-xl font-bold text-purple-400 uppercase">KevinSocial</h2>
         </Link>
-        
+
         <Links />
+        <SearchBar />
     </nav>
   </header>
 }
---
+++ src/components/headers/SearchBar.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { Search } from "lucide-react"
+import {Input} from "@/components/ui/input"
+
+export const SearchBar = () => {
+  return <div className="flex items-center gap-2 w-[200px] bg-[#121212] rounded-lg px-3">
+      <Input type="text" placeholder="Search..." className="bg-transparent border-none focus:outline-none"/>
+      <Search size={18} className="cursor-pointer"/>
+    </div>
+}
