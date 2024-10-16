b8a537f07769b6d595e5b0663b9bcf34dc7b5bea
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-16 23:50:51 +0200

    stories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 import { Stories } from "@/components/mainHome/Stories";
 import { SidebarLeft } from "@/components/sidebarLeft/SidebarLeft";
 import { SidebarRight } from "@/components/sidebarRight/SidebarRight";
+import { getSession } from "@/components/utils/CacheSession";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getSession()
+
   return (
     <div className="flex max-w-[1500px] mx-auto gap-8 my-5">
       <SidebarLeft />
+
       <main className="w-full xl:w-[55%]">
-        <Stories />
+        <Stories sessionImage={session?.user?.image || ""} />
       </main>
+
      <SidebarRight />
     </div>
   );
--- src/components/mainHome/Stories.tsx
+++ src/components/mainHome/Stories.tsx
@@ -1,11 +1,18 @@
 import { Plus } from "lucide-react"
 
-export const Stories = () => {
+export const Stories = ({sessionImage}: {sessionImage: string}) => {
   return <div className="w-full shadow-md gap-5 bg-white rounded-md p-4 flex items-center justify-between overflow-x-auto">
         {/* add story */}
         <div className="flex items-center flex-col gap-3 cursor-pointer">
-          <div className="w-[90px] h-[90px] rounded-full bg-blue-200 flex items-center justify-center">
-            <Plus size={40} className="text-blue-500" />
+          <div className="w-[90px] h-[90px] rounded-full relative overflow-hidden">
+            <img
+              src={sessionImage}
+              alt="User profile"
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30">
+              <Plus size={40} className="text-gray-200" />
+            </div>
           </div>
           <span className="text-sm font-semibold">Add a Story</span>
         </div>
