0c711cede2a75cd420695f0dc839ea0ce9a6d10b
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-17 16:45:21 +0200

    comments

--- src/components/posts/CommentForm.tsx
+++ src/components/posts/CommentForm.tsx
@@ -20,7 +20,12 @@ export const CommentForm = ({postId, userImage}: {postId: string, userImage: str
   return <div className="flex items-center gap-4 mt-8">
     <img src={userImage} alt="user image" className="w-8 h-8 rounded-full" />
     <form onSubmit={handleSubmit(onSubmit)} className="flex items-center gap-3 w-full bg-gray-100 p-1 rounded-lg">
-        <Input type="text" placeholder="Add a comment" {...register("comment")} className="bg-transparent border-none focus:outline-none" />
+        <Input
+          type="text"
+          placeholder="Add a comment"
+          {...register("comment", { required: true, minLength: 4, maxLength: 200 })}
+          className="bg-transparent border-none focus:outline-none"
+        />
         <span className="text-md mr-5 cursor-pointer">😃</span>
     </form>
     <ToastContainer />
