07897d240dd43d281294a5b8d7177fa4b48480d1
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-17 16:44:51 +0200

    comments

--- src/components/posts/CommentForm.tsx
+++ src/components/posts/CommentForm.tsx
@@ -1,15 +1,28 @@
 "use client"
 import { useForm } from "react-hook-form"
 import { Input } from "@/components/ui/input"
+import { commentPost } from "@/server/CommentPost.action"
+import {ToastContainer, toast} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 export const CommentForm = ({postId, userImage}: {postId: string, userImage: string}) => {
   const { register, handleSubmit } = useForm()
 
+  const onSubmit = async (data: any) => {
+    try{
+      await commentPost({postId, content: data.comment})
+      toast.success("Commentaire ajouté")
+    }catch(error){
+      toast.error("Erreur lors de l'ajout du commentaire")
+    }
+  }
+
   return <div className="flex items-center gap-4 mt-8">
     <img src={userImage} alt="user image" className="w-8 h-8 rounded-full" />
-    <form className="flex items-center gap-3 w-full bg-gray-100 p-1 rounded-lg">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex items-center gap-3 w-full bg-gray-100 p-1 rounded-lg">
         <Input type="text" placeholder="Add a comment" {...register("comment")} className="bg-transparent border-none focus:outline-none" />
         <span className="text-md mr-5 cursor-pointer">😃</span>
     </form>
+    <ToastContainer />
   </div>
 }
