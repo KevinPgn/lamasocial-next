3e3575da3c1633eaa2bd09d4afae74671381d53b
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-17 18:32:55 +0200

    ellipsis

--- src/components/posts/Ellipsis.tsx
+++ src/components/posts/Ellipsis.tsx
@@ -1,8 +1,16 @@
 "use client"
 import {Ellipsis as EllipsisIcon} from "lucide-react"
+import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu"
 
-export const Ellipsis = () => {
-  return <>
-    <EllipsisIcon size={20} className="absolute top-5 right-5 cursor-pointer" />
-  </>
+export const Ellipsis = ({postId}: {postId: string}) => {
+  return <DropdownMenu>
+    <DropdownMenuTrigger>
+      <EllipsisIcon size={20} className="cursor-pointer" />
+    </DropdownMenuTrigger>
+    <DropdownMenuContent className="w-40 rounded-xl px-2 py-3">
+      <DropdownMenuItem className="cursor-pointer font-semibold">View</DropdownMenuItem>
+      <DropdownMenuItem className="cursor-pointer font-semibold">Re-post</DropdownMenuItem>
+      <DropdownMenuItem className="text-red-500 cursor-pointer font-semibold">Delete</DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
 }
--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -9,13 +9,15 @@ import Link from "next/link"
 
 export const Post = ({post, currentUserConnected}: {post: any, currentUserConnected: any}) => {
   return <div className="w-full relative shadow-md gap-5 bg-white rounded-lg p-4 my-8">
-    {currentUserConnected.user.id === post.author.id ? <Ellipsis /> : null}
-    <div className="flex items-center gap-3 mb-2">
-        <img src={post.author.image} alt="User profile" className="w-9 h-9 rounded-full" />
-        <div>
-            <h1 className="text-md font-semibold">{post.author.name}</h1>
-            <p className="text-sm text-gray-500">{formatDistance(new Date(post.createdAt), new Date(), { addSuffix: true })}</p>
+    <div className="flex items-center justify-between mb-2">
+        <div className="flex items-center gap-3">
+            <img src={post.author.image} alt="User profile" className="w-9 h-9 rounded-full" />
+            <div>
+                <h1 className="text-md font-semibold">{post.author.name}</h1>
+                <p className="text-sm text-gray-500">{formatDistance(new Date(post.createdAt), new Date(), { addSuffix: true })}</p>
+            </div>
         </div>
+        {currentUserConnected.user.id === post.author.id ? <Ellipsis postId={post.id}/> : null}
     </div>
     {post.title ? <h1 className="text-lg font-semibold my-3">{post.title}</h1> : null}
     {post.image ? <img src={post.image} alt="Post image" loading="lazy" className="w-full h-[450px] object-cover my-3 rounded-lg" /> : null}
