9ceb84a25ce7b3a5e3fdf4856fc7aa5f656365d5
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-17 14:29:00 +0200

    like ost

--- src/components/posts/LikePost.tsx
+++ src/components/posts/LikePost.tsx
@@ -15,7 +15,7 @@ export const LikePost = ({postId, isLiked, postLikes}: {postId: string, isLiked:
     <div
     onClick={handleLike}
     className={cn("flex items-center gap-2 p-2 px-3 rounded-lg cursor-pointer duration-75", optimisticLike ? "bg-orange-200" : "bg-red-100")}>
-      <Flame size={20} className={cn("text-red-500", optimisticLike ? "text-orange-500" : "")} />
+      <Flame size={20} className={cn("text-red-500", optimisticLike ? "text-orange-500 fill-orange-500" : "")} />
       <div className={cn("w-[1px] h-[20px]", optimisticLike ? "bg-orange-500" : "bg-red-700")}></div>
       <span className={cn("text-sm font-semibold", optimisticLike ? "text-orange-500" : "text-red-500")}>{postLikes} likes</span>
     </div>
