ef53fb5eda8280860b1e7d2ae24d85110eee4e3e
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-18 00:13:56 +0200

    profile

--- src/app/profile/[userId]/page.tsx
+++ src/app/profile/[userId]/page.tsx
@@ -1,7 +1,10 @@
 import { SidebarLeft } from '@/components/sidebarLeft/SidebarLeft'
 import { SidebarRight } from '@/components/sidebarRight/SidebarRight'
+import { getUserProfileWithPosts } from '@/server/GetUserProfile.action'
 
-const UserProfilePage = () => {
+const UserProfilePage = async ({ params }: { params: { userId: string } }) => {
+  const { profile, posts } = await getUserProfileWithPosts(params.userId)
+
   return (
     <div className="flex max-w-[1500px] mx-auto gap-8 my-5">
       <SidebarLeft />
