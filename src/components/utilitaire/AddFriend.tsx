446e9eb5709aac5c73a248e14efa9c72a86de37d
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-18 14:02:38 +0200

    add friends

--- src/components/utilitaire/AddFriend.tsx
+++ src/components/utilitaire/AddFriend.tsx
@@ -1,12 +1,19 @@
 import { Button } from "@/components/ui/button"
-import { sendFriendRequest } from "@/server/FriendShip.action"
+import { deleteTheFriendship, sendFriendRequest } from "@/server/FriendShip.action"
 
 export const AddFriend = ({userId, isFriend}: {userId: string, isFriend: boolean}) => {
   const handleAddFriend = async () => {
     await sendFriendRequest({userId})
   }
+  const handleRemoveFriend = async () => {
+    await deleteTheFriendship({userId})
+  }
+
+  if(isFriend) {
+    return <Button className="w-[50%]" onClick={handleRemoveFriend}>Remove Friend</Button>
+  }
   
   return <>
-    <Button className="w-[50%]" onClick={handleAddFriend}>{isFriend ? "Remove Friend" : "Add Friend"}</Button>
+    <Button className="w-[50%]" onClick={handleAddFriend}>Add Friend</Button>
   </>
 }
