379103f1a31f7db9fdd8e2df651dc672d01e6ce0
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-21 21:17:13 +0200

    mises a jour

--- src/app/profile/[userId]/page.tsx
+++ src/app/profile/[userId]/page.tsx
@@ -4,9 +4,16 @@ import { getUserProfileWithPosts } from '@/server/GetUserProfile.action'
 import { getSession } from '@/components/utils/CacheSession'
 import { ProfileHome } from '@/components/profileHome/ProfileHome'
 
-const UserProfilePage = async ({ params }: { params: { userId: string } }) => {
+interface UserProfilePageProps {
+  params: Promise<{
+    userId: string
+  }>
+}
+
+const UserProfilePage = async (props: UserProfilePageProps) => {
+  const { userId } = await props.params
   const session = await getSession()
-  const { profile, posts, media } = await getUserProfileWithPosts(params.userId)
+  const { profile, posts, media } = await getUserProfileWithPosts(userId)
 
   return (
     <div className="flex max-w-[1500px] mx-auto gap-8 my-5">
